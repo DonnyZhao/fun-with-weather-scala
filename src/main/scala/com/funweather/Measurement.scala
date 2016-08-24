b9c9a81a4395d5a57beb4ed8ec518e2a7d1a9cb7
Lei Shang <lei.shang@hotmail.com>
2016-08-24 18:50:32 +1000

    Minor changes

--- src/main/scala/com/funweather/Measurement.scala
+++ src/main/scala/com/funweather/Measurement.scala
@@ -8,15 +8,15 @@ import com.funweather.Condition.Condition
   * A container for measurement in the format of
   * Location Position Local Time Conditions Temperature Pressure Humidity
   * example:
-  * Sydney|-33.86,151.21,39|2015-12-23T05:02:12Z|Rain|+12.5|1004.3|97
+  * Sydney|-33.86,151.21,39|2015-12-23T05:02:12|Rain|+12.5|1004.3|97
   *
-  * @param number Measurement number or id
-  * @param position Position of the measurement containing latitude, longitude and elevation
-  * @param location Location of the measurement
-  * @param condition Weather condition
+  * @param number      Measurement number or id
+  * @param position    Position of the measurement containing latitude, longitude and elevation
+  * @param location    Location of the measurement
+  * @param condition   Weather condition
   * @param temperature Weather sensor measurement
-  * @param pressure Weather sensor measurement
-  * @param humidity Weather sensor measurement
+  * @param pressure    Weather sensor measurement
+  * @param humidity    Weather sensor measurement
   */
 case class Measurement(number: Int,
                        position: Position, localTime: LocalTime,
