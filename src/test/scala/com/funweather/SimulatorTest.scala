9ea8ae3c92488785bdc34c0f5cde63fefa5ab5d8
Lei Shang <lei.shang@hotmail.com>
2016-08-26 09:50:49 +1000

    A bit more detailed tests for the established models

--- src/test/scala/com/funweather/SimulatorTest.scala
+++ src/test/scala/com/funweather/SimulatorTest.scala
@@ -1,17 +1,15 @@
 package com.funweather
 
+import org.apache.spark.mllib.tree.configuration.Algo
 import org.apache.spark.mllib.tree.model.RandomForestModel
+
 
 /**
   * Created by lshang on 8/23/16.
   */
 class SimulatorTest extends UnitTest("Simulator") {
 
-  it should "build and return four Random Forest models, also generate a sample with a new time&location pair" in {
-    val models = Simulator.buildModels()
-    models.size shouldEqual 4
-    models.values.foreach(model => model.isInstanceOf[RandomForestModel] shouldEqual true)
-
+  it should "generate a sample with a new time&location pair" in {
     val sample = Simulator.generateSample(LocalTime("2016-08-22T12:00:00"), Position(-33.86, 151.21, 39.00))
     sample.size shouldEqual 4
 
@@ -19,6 +17,18 @@ class SimulatorTest extends UnitTest("Simulator") {
     sample("Temperature").isInstanceOf[Double] shouldEqual true
     sample("Pressure").isInstanceOf[Double] shouldEqual true
     sample("Humidity").isInstanceOf[Double] shouldEqual true
+  }
+
+  it should "build and return one classification model, three regression models" in {
+    val models = Simulator.buildModels()
+    models.size shouldEqual 4
+
+    models.values.foreach(model => model.isInstanceOf[RandomForestModel] shouldEqual true)
+
+    models("Condition").algo.compare(Algo.Classification) shouldEqual 0
+    models("Temperature").algo.compare(Algo.Regression) shouldEqual 0
+    models("Pressure").algo.compare(Algo.Regression) shouldEqual 0
+    models("Humidity").algo.compare(Algo.Regression) shouldEqual 0
   }
 
 }
