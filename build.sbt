acab94d6fb7f2750c3d98bcb049ed0e2d0aac9e2
Lei Shang <lei.shang@hotmail.com>
2016-09-09 10:58:20 +1000

    Use probability monad for weather condition generation

--- build.sbt
+++ build.sbt
@@ -9,6 +9,7 @@ parallelExecution in Test := false
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "2.2.4" % "test",
   "org.scalanlp" %% "breeze" % "0.12",
+  "org.jliszka" %% "probability-monad" % "1.0.1",
   "org.apache.spark" % "spark-core_2.11" % "2.0.0",
   "org.apache.spark" % "spark-mllib_2.11" % "2.0.0"
 )
---
+++ src/main/scala/com/probability/MarkovChainExamples.scala
@@ -0,0 +1,29 @@
+package com.probability
+
+import probability_monad.Distribution
+import probability_monad.Distribution._
+
+/**
+  * Created by lshang on 9/5/16.
+  *
+  * Example to generate a Markov Chain for weather condition
+  */
+object MarkovChainExamples extends App {
+
+  def transition(condition: Int): Distribution[Int] = condition match {
+    case 0 => discrete(0 -> 1 / 3, 1 -> 1 / 3, 2 -> 1 / 3)
+    case 1 => discrete(0 -> 0.1, 1 -> 0.8, 2 -> 0.1)
+    case 2 => discrete(0 -> 0.99, 1 -> 0.01, 2 -> 0)
+  }
+
+  def chain(num: Int): Distribution[List[Int]] = {
+    val first = 0 // can be random
+    always(List(first)).markov(num - 1)(sequence => for {
+      next <- transition(sequence.last)
+    } yield sequence :+ next)
+  }
+
+  val weatherCondition = chain(30)
+  println(weatherCondition.sample(1).flatten)
+
+}
